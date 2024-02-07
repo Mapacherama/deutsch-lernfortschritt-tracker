74c23fa944b955f4bb18c0142da6fc9c8903e5bd
Jerome Tesselaar <jtesselaar@snelstart.nl>
2024-02-07 08:17:02 +0100

    Feature: Update the .Gitignore, and made the Dashboard/homepage responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  // Beispiel Daten
+  gelernteWoerter = 150;
+  abgeschlosseneLektionen = 10;
+  verbrachteLernzeit = '5 Stunden'; // Im realen Szenario, berechnen basierend auf tatsächlichen Daten
+  fortschrittsStatistiken = {
+    grammatik: 70, // Prozent
+    wortschatz: 60, // Prozent
+    aussprache: 80, // Prozent
+  };
+
+  // Daten für zuletzt gelernte Lektionen und empfohlene Lektionen
+  zuletztGelernteLektionen = [
+    { name: 'Einführung in die Grammatik', beschreibung: 'Grundlagen der deutschen Grammatik.' },
+    { name: 'Wortschatz Aufbau', beschreibung: 'Erweiterung des Vokabulars.' }
+  ];
+
+  empfohleneLektionen = [
+    { name: 'Fortgeschrittene Grammatik', beschreibung: 'Komplexe Satzstrukturen.' },
+    { name: 'Aussprache Training', beschreibung: 'Verbesserung der Aussprache.' }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
