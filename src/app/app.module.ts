c23c6265df418a7f44195dbf2972b4a562f2b5a3
Jerome Tesselaar <jtesselaar@snelstart.nl>
2024-02-09 14:14:59 +0100

    Feature: Added routing to the header, and put the footer on the bottom of the page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FortschrittsanzeigeComponent } from './fortschrittsanzeige/fortschrittsanzeige.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { GermanMusicComponent } from './german-music/german-music/german-music.component';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { GermanAudiobooksComponent } from './german-audiobooks/german-audiobooks.component';
 import { GermanHistoryComponent } from './german-history/german-history.component';
 import { GermanTagebuchComponent } from './german-tagebuch/german-tagebuch.component';
 import { AboutComponent } from './about/about.component';
+import { GermanMusicComponent } from './german-music/german-music.component';
+import { FortschrittsanzeigeComponent } from './fortschrittsanzeige/fortschrittsanzeige.component';
+
 
 @NgModule({
   declarations: [
@@ -30,8 +35,10 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MatProgressBarModule,
+    MatButtonModule,
     MatToolbarModule
   ],
   providers: [],
