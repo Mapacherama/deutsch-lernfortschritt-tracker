3e15fc382a9dcf1d62a84dde0cc768ebc4f4ad55
Jerome Tesselaar <jtesselaar@snelstart.nl>
2024-02-21 14:36:39 +0100

    Additional setting up of the templates and models

--- src/app/german-audiobooks/german-audiobooks.component.ts
+++ src/app/german-audiobooks/german-audiobooks.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+// src/app/german-audiobooks/german-audiobooks.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { Audiobook } from '../models/audiobook.model';
 
 @Component({
   selector: 'app-german-audiobooks',
   templateUrl: './german-audiobooks.component.html',
   styleUrls: ['./german-audiobooks.component.css']
 })
-export class GermanAudiobooksComponent {
+export class GermanAudiobooksComponent implements OnInit {
+  audiobooks: Audiobook[] = [
+    {
+      id: '1',
+      title: 'Der Audiobook Titel 1',
+      author: 'Autor 1',
+      description: 'Eine kurze Beschreibung des Audiobooks 1.',
+      link: 'https://linktoaudiobook1.com'
+    },
+    {
+      id: '2',
+      title: 'Der Audiobook Titel 2',
+      author: 'Autor 2',
+      description: 'Eine kurze Beschreibung des Audiobooks 2.',
+      link: 'https://linktoaudiobook2.com'
+    },
+    // Add more audiobooks as needed
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
---
+++ src/app/models/audiobook.model.ts
@@ -0,0 +1,10 @@
+// src/app/models/audiobook.model.ts
+
+export interface Audiobook {
+    id: string;
+    title: string;
+    author: string;
+    description: string;
+    link: string; // URL to the audiobook
+  }
+  
