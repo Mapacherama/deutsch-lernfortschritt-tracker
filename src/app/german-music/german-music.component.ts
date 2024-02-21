5f53ac86b2f9ddeb17f7f75f5008d78305ea3919
Jerome Tesselaar <jtesselaar@snelstart.nl>
2024-02-21 14:06:30 +0100

    Setting up more of the models and the templates

--- src/app/german-music/german-music.component.ts
+++ src/app/german-music/german-music.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+// src/app/german-music/german-music.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { SpotifyPlaylist } from '../models/spotify-playlist.model';
 
 @Component({
   selector: 'app-german-music',
   templateUrl: './german-music.component.html',
   styleUrls: ['./german-music.component.css']
 })
-export class GermanMusicComponent {
+export class GermanMusicComponent implements OnInit {
+  playlists: SpotifyPlaylist[] = [
+    {
+      id: '1',
+      name: 'German Pop Hits',
+      description: 'The latest hits from the German pop scene.',
+      spotifyLink: 'https://open.spotify.com/playlist/yourplaylistlink1'
+    },
+    {
+      id: '2',
+      name: 'German Classics',
+      description: 'Classic tunes from Germany.',
+      spotifyLink: 'https://open.spotify.com/playlist/yourplaylistlink2'
+    },
+    // Add more playlists as needed
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
---
+++ src/app/models/spotify-playlist.model.ts
@@ -0,0 +1,7 @@
+export interface SpotifyPlaylist {
+    id: string;
+    name: string;
+    description: string;
+    spotifyLink: string;
+  }
+  
