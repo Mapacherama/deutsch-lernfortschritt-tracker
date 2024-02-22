3397d9633307dc73b4d493b3934a50f0a17324bf
Jerome Tesselaar <jtesselaar@snelstart.nl>
2024-02-22 08:24:40 +0100

    Feature: Adding TailwindCSS to the project, and adding image to the Dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './src/**/*.{html,ts}', // This path covers your Angular component templates and TypeScript files
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
